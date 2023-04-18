5b8afb76f8530b4daddb845c7561e6167224448b
ParkSungHyon <shp234234@gmail.com>
2023-04-18 20:50:03 +0900

    Update news_crawaling4.py

--- news_crawaling4.py
+++ news_crawaling4.py
@@ -9,37 +9,40 @@ chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 browser = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
 
-url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=101&sid2=259"
-browser.get(url)
+for i in range(1,4):
+    url = "https://news.naver.com/main/list.naver?mode=LS2D&sid2=259&sid1=101&mid=shm&date=20230418&page={i}"
+    browser.get(url)
 
-#####attribte를 호출하기 위해서는 같은 < > 안에 있어야 한다.
-### 같은 페이지에서 반복 횟수는 어떻게 정하나?
+    #####attribte를 호출하기 위해서는 같은 < > 안에 있어야 한다.
+    ### 같은 페이지에서 반복 횟수는 어떻게 정하나?
 
-news = browser.find_element(By.CLASS_NAME, 'content')
-news_a = news.find_elements(By.TAG_NAME, 'a') # 제목
-news_lede = news.find_elements(By.CLASS_NAME, 'lede') # 내용
-news_wr = news.find_elements(By.CLASS_NAME, 'writing') # 신문사
+    news = browser.find_element(By.CLASS_NAME, 'content')
+    news_a = news.find_elements(By.TAG_NAME, 'a') # 제목
+    news_lede = news.find_elements(By.CLASS_NAME, 'lede') # 내용
+    news_wr = news.find_elements(By.CLASS_NAME, 'writing') # 신문사
 
-title = []
-summary = []
-link = []
-company = []
+    title = []
+    summary = []
+    link = []
+    company = []
 
 
-for i in news_a:
-    title.append(i.text)
-    
-for i in news_lede:
-    summary.append(i.text)
-       
-for i in news_a:
-    link.append(i.get_attribute('href'))
+    for i in news_a:
+        title.append(i.text)
+        
+    for i in news_lede:
+        summary.append(i.text)
+        
+    for i in news_a:
+        link.append(i.get_attribute('href'))
 
-for i in news_wr:
-    company.append(i.text)
+    for i in news_wr:
+        company.append(i.text)
 
-for i in range(0,20):
-    print(f"제목{i} : " + title[i])
-    print("내용 : " + summary[i])
-    print("링크 : " + link[i])
-    print("신문사 : " + company[i] + "\n")
+    for i in range(0,20):
+        print(f"제목{i} : " + title[i])
+        print("내용 : " + summary[i])
+        print("링크 : " + link[i])
+        print("신문사 : " + company[i] + "\n")
+
+browser.close()
